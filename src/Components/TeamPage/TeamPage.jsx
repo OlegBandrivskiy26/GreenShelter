5f1853e45ad48eab1529474cdac6163a930f1829
Viktor Kindrat <kindratvictor07@gmail.com>
2023-03-26 18:04:50 +0300

    add TeamPage component

---
+++ src/Components/TeamPage/TeamPage.jsx
@@ -0,0 +1,11 @@
+import "./TeamPage.css"
+
+function TeamPage (props) {
+    return (
+        <div className="TeamPage">
+            <h2>Наша Команда</h2>
+        </div>
+    )
+}
+
+export default TeamPage

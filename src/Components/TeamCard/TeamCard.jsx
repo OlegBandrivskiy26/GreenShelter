255f63508b595072817d28fd2875d1fbd69fa115
Viktor Kindrat <kindratvictor07@gmail.com>
2023-03-26 18:50:24 +0300

    TeamCard update. Create images folder in public

--- src/Components/TeamCard/TeamCard.jsx
+++ src/Components/TeamCard/TeamCard.jsx
@@ -1,12 +1,14 @@
 import "./TeamCard.css"
+import instagramIcon from "./img/instagram.svg"
+import facebookIcon from "./img/facebook.svg"
 
 function TeamCard(props) {
     return (
         <div className="TeamCard">
             <img src={props.image} alt="Team member" className="TeamCard-image" />
             <div className="TeamCard-medias">
-                <a href="/" className="TeamCard-media-btn"></a>
-                <a href="/" className="TeamCard-media-btn"></a>
+                <a href="/" className="TeamCard-media-btn"><img src={instagramIcon} alt="instagram" className="TeamCard-mediaIcon" /></a>
+                <a href="/" className="TeamCard-media-btn"><img src={facebookIcon} alt="facebook" className="TeamCard-mediaIcon" /></a>
             </div>
         </div>
     )
